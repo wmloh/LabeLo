a6a4dc59006803854b28ca973c438983563fc0f6
Wei Min Loh <32558880+wmloh@users.noreply.github.com>
2018-09-15 15:58:40 -0400

    constructed Player database, and improved organization

--- backend/server.py
+++ backend/server.py
@@ -7,28 +7,18 @@ from hashlib import sha256
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'db1': app.config['SQLALCHEMY_DATABASE_URI'],
+    'db2': 'sqlite:////tmp/test2.db'
+    }
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-class Label(db.Model):
-    id = db.Column(db.String(128), primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    label = db.Column(db.String(40), unique=True, nullable=False)
-
-    def __repr__(self):
-        return self.label
-
+### Default Routes
 @app.route('/')
 def test():
-    return 'upload, ' + 'sendstr<string>, ' + ', sendimg' + ', getlabels'
+    return 'General: upload\n' +\
+           'Label: getimg/(name), sendlabel/(param), getlabels\n'  + \
+           'Player: add/(name), update/(param), refresh/(player)'
 
 @app.route('/upload')
 def upload_html():
@@ -40,23 +30,31 @@ def upload_file():
     filename = file.save(file.filename)
     return 'Filed saved'
 
-@app.route('/sendstr/<string>')
-def send_str(string):
-    return 'Received ' + string
+### Label Routes
+class Label(db.Model):
+    __bind_key__ = 'db1'
+    id = db.Column(db.String(128), primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    label = db.Column(db.String(40), unique=False, nullable=False)
+    player = db.Column(db.String(40), unique=False, nullable=False)
 
-@app.route('/getimg')
-def get_img():
-    return send_file('./img/cat.1.jpg', mimetype='image/jpg')
+    def __repr__(self):
+        return self.name
+
+@app.route('/getimg/<img>')
+def get_img(img):
+    return send_file('img/' + img, mimetype='image/jpg')
 
 @app.route('/sendlabel/<param>')
 def send_label(param):
-    img, lab = param.split('=')
+    img, lab, plyr = param.split('=')
     img = img.encode('utf-8')
     lab = lab.encode('utf-8')
+    plyr = plyr.encode('utf-8')
     hash_str = sha256(img).hexdigest()
-    db.session.add(Label(id=hash_str, name=img, label=lab))
+    db.session.add(Label(id=hash_str, name=img, label=lab, player=plyr))
     db.session.commit()
-    return 'Received %s; %s; %s' % (hash_str, img, lab)
+    return 'Received %s; %s; %sl %s' % (hash_str, img, lab, plyr)
 
 @app.route('/getlabels', methods=['GET'])
 def get_labels():
@@ -65,9 +63,50 @@ def get_labels():
         data = json.load(f)
     return jsonify(data)
 
+### Player Routes
+class Player(db.Model):
+    __bind_key__ = 'db2'
+    id = db.Column(db.String(128), primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    correct = db.Column(db.Integer)
+    wrong = db.Column(db.Integer)
+    rank = db.Column(db.Integer)
+
+    def __repr__(self):
+        return self.name
+
+@app.route('/add/<name>')
+def add_player(name):
+    hash_str = sha256(name).hexdigest()
+    db.session.add(Player(id=hash_str, name=name, correct=0, wrong=0, rank=0))
+    db.session.commit()
+    return 'Received %s; %s; %i; %i; %i' % (hash_str, name,
+                                            correct, wrong, rank)
+
+@app.route('/update/<param>')
+def update_player(param):
+    name, val = param.split('=')
+    player = Player.query.filter_by(name=name).first()
+    if val == '+1':
+        player.correct += 1
+    elif val == '-1':
+        player.wrong += 1
+    else:
+        raise ValueError('Check the parameters of the route.')
+    db.session.commit()
+    
+    return 'Current correct: %i, wrong: %i' % (player.correct, player.wrong)
+
+@app.route('/refresh/<name>')
+def refresh_player(name):
+    player = Player.query.filter_by(name=name).first()
+    player.correct = 0
+    player.wrong = 0
+
+    db.session.commit()
+    return 'Refreshed successfully'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-db.create_all()
 
 #35.185.87.150
